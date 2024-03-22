010328521a9c501a8b0b200e514755a2aa94316e
Cristian0-Pereira <cristianops2020@gmail.com>
2024-03-21 22:32:47 -0300

    ad

--- 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
+++ 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
@@ -1,11 +1,16 @@
 salario = 2000
 
-
 def salario_bonus(bonus):
     global salario
+
+    lista_aux  = lista.copy()
+    lista_aux.append(2)
+    print(f'lista aux = {lista_aux}')
+
     salario += bonus
     return salario
 
-
+lista = [1]
 novo_salario = salario_bonus(500)  # 2500
 print(novo_salario)
+print(lista)
